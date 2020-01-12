bd58910ad0fb025b7db7d4960e816ab730950aec
Ace Petrucci <ace.petrucci@outlook.com>
2020-01-12 04:57:21 -0300

    Style: Remove .js and .js.map

--- src/app/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-const inversify_config_1 = require("../config/container/inversify.config");
-const types_1 = require("../config/typings/types");
-const trashBot = inversify_config_1.default.get(types_1.TYPES.TrashBot);
-trashBot.listen().then(() => console.log('Logged in!')).catch((error) => console.log('RIP', error));
-//# sourceMappingURL=index.js.map
--- src/app/utils/keywords/channels.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.channels = [
-    'bot',
-    'bots',
-    'bot-spam',
-    'bots-spam'
-];
-//# sourceMappingURL=channels.js.map
