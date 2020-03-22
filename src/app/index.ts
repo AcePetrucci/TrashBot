43438d34b885a7d6ee939caa7f2186ded2355f77
Andre Petrucci <ace.petrucci@outlook.com>
2020-03-22 05:48:32 -0300

    add http port, add env prod

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,10 +1,18 @@
 require('dotenv').config();
 
+const http = require('http');
+
 import container from '../config/container/inversify.config';
 import { TYPES } from '../config/typings/types';
 
 import { TrashBot } from './components/main/main.bot';
 
 const trashBot = container.get<TrashBot>(TYPES.TrashBot);
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+});
 
-trashBot.listen().then(() => console.log('Logged in!')).catch((error) => console.log('RIP', error));
+server.listen(8080, '127.0.0.1', () => {
+  console.log('Listening to 127.0.0.1:8080');
+  trashBot.listen().then(() => console.log('Logged in!')).catch((error) => console.log('RIP', error));
+});
